1bfd31e8e7d1d8d8677ca53aaa47d1b7749d3bfc
jamesanguyen <nguyen.james115@gmail.com>
2024-12-18 12:33:38 -0800

    remove old files, add updated function

--- numCorrect.m
+++
@@ -1,3 +0,0 @@
-function ans = numCorrect(vec1, vec2)
-    ans = sum(vec1 == vec2);
-end
--- readScore.m
+++
@@ -1,8 +0,0 @@
-function [rank] = readScore(score)
-    n = length(score);
-    rank = zeros(1, n); % initialize
-    [~, index] = sort(score, 'descend'); % sort in descending order
-    for i = 1:n
-        rank(index(i)) = i;
-    end
-end
